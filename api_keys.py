d05a34c190b03ea44532bc8c80c40819c47d9601
shantipriya30 <75239249+shantipriya30@users.noreply.github.com>
2021-03-09 20:50:23 -0800

    Initial twitch api setup

---
+++ api_keys.py
@@ -0,0 +1,4 @@
+client_id = "kzaqq2pu31fdkf7pairl9brbl6xqbz"
+# client_secret = "nz7y1s37s7dprrpb2wgujr1tbmnn7p"
+
+# wrapper = IGDBWrapper("kzaqq2pu31fdkf7pairl9brbl6xqbz", "nz7y1s37s7dprrpb2wgujr1tbmnn7p")

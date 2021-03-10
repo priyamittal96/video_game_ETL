1abf17ec52ca6d477e73c6d9c287728c5d0e7459
Priya Mittal <75239249+priyamittal96@users.noreply.github.com>
2021-03-09 21:19:08 -0800

    Incorrect keys

--- api_keys.py
+++ api_keys.py
@@ -1,4 +1,2 @@
-client_id = "kzaqq2pu31fdkf7pairl9brbl6xqbz"
-# client_secret = "nz7y1s37s7dprrpb2wgujr1tbmnn7p"
+client_id = ""
 
-# wrapper = IGDBWrapper("kzaqq2pu31fdkf7pairl9brbl6xqbz", "nz7y1s37s7dprrpb2wgujr1tbmnn7p")
